19edc4a4aa6668e6e134eb7b0f3244035c687a7a
Stone-People-Like <19243004532@163.com>
2026-08-27 19:13:08 +0800

    HelloWord2解释 hello word 程序

--- src/stone/test/HelloWord.java
+++ src/stone/test/HelloWord.java
@@ -1,7 +1,8 @@
 package Stone.test;
 
 public class HelloWord {
-public static void main(String[] args){
-    System.out.println("HelloWord");
-}
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+//     我是注释
+    }
 }
