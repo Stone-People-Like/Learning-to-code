35fc998e7e853fa6947dae1495040708eeedec56
Stone-People-Like <19243004532@163.com>
2026-08-28 12:48:24 +0800

    开始速刷

--- src/stone/test/HelloWord2.java
+++ src/stone/test/HelloWord2.java
@@ -5,8 +5,9 @@ package Stone.test;
 //定义一个类
 public class HelloWord2 {
 
-//    表示 java 程序的主入口
+    //    表示 java 程序的主入口
     public static void main(String[] args) {
         System.out.println("HelloWord2");
     }
+
 }
