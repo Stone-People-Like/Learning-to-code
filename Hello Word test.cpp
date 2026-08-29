0a97ce58687743ef1dabb9bf9ffd3af3e9fa2a72
Stone-People-Like <19243004532@163.com>
2026-08-30 02:21:16 +0800

    如何不冲突的情况下运行多个 main 的 cpp 文件

---
+++ Hello Word test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Admin on 2026/8/30.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Word" << endl;
+    return 0;
+}
--- Hello_Word.cpp
+++ Hello_Word.cpp
@@ -4,4 +4,5 @@ using namespace std;
 
 int main() {
     cout << "I Love C++" << endl;
+    return 0;
 }
