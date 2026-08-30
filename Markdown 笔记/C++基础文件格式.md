89531226dfd42e1a4285c59db62649fc9d5c7813
Stone-People-Like <19243004532@163.com>
2026-08-30 12:13:29 +0800

    新增 C++基础文件格式.md

---
+++ Markdown 笔记/C++基础文件格式.md
@@ -0,0 +1,14 @@
+# C++基础文件格式
+
+```c++
+
+#include "iostream"   //必写文件
+using namespace std;  //必写文件
+
+int main() {   //程序主入口
+    //下面开始写程序
+    return 0;
+}
+ 
+```
+
---
+++ cout打印输出.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 2026/8/30.
+//
+
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    cout << 13 << "测试" << 24 << endl;
+    cout << "I Love C++" << endl;
+    return 0;
+}
